53ef10942c507f763e7dc8dac611c031f2dce911
Yurii BUHAI <ybuhai@e2r14p2.unit.ua>
2019-03-28 13:17:56 +0200

    print stage

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ybuhai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/24 18:11:07 by ybuhai            #+#    #+#             */
-/*   Updated: 2019/03/28 12:54:32 by ybuhai           ###   ########.fr       */
+/*   Updated: 2019/03/28 13:16:32 by ybuhai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,25 @@ void	map_init(t_cor *cor)
 	cor->list = NULL;
 	heroes_to_null(cor);
 }
+void		print_arena(uint8_t *arena, int print_mode)
+{
+	int	i;
+	int	j;
 
+	i = 0;
+	while (i < MEM_SIZE)
+	{
+		ft_printf("%.4p : ", i);
+		j = 0;
+		while (j < print_mode)
+		{
+			ft_printf("%.2x ", arena[i + j]);
+			j++;
+		}
+		ft_printf("\n");
+		i += print_mode;
+	}
+}
 void	print_data(t_cor *cor)
 {
 	int i;
@@ -46,6 +64,7 @@ void	print_data(t_cor *cor)
 	i = -1;
 	while (++i < cor->count_heroes)
 		ft_printf("Hero %i file %s\nname \"%s\"\ncomment (\"%s\")\nsize %i\n\n\n\n", i + 1, cor->heroes[i].file, cor->heroes[i].name, cor->heroes[i].comment, cor->heroes[i].size);
+	print_arena(cor->stage, 32);
 }
 
 int		main(int argc, char **argv)
